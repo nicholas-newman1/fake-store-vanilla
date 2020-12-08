83f1fd3310d247cdd7c7024baf17739b9a024934
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-08 05:46:52 -0500

    added loading component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
 import Error404Screen from './screens/Error404Screen.js';
+import Loading from './components/Loading.js';
 import Header from './components/Header.js';
 import { parseRequestUrl } from './utils.js';
 
@@ -17,11 +18,12 @@ const router = async () => {
     (request.verb ? `/${request.verb}` : '');
   const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen;
 
-  const main = document.getElementById('root');
-  main.innerHTML = await screen.render();
-
   const header = document.getElementById('header');
   header.innerHTML = Header.render();
+
+  const main = document.getElementById('root');
+  main.innerHTML = Loading.render();
+  main.innerHTML = await screen.render();
 };
 
 window.addEventListener('load', router);
