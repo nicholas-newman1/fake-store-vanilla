ffc850840bf40183721522561a34ecb983f274c1
Nicholas Newman <nicholas.newman1@hotmail.com>
2020-12-09 07:20:16 -0500

    added after_render to HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import Products from '../components/Products.js';
 
 const HomeScreen = {
+  after_render: () => {
+    Products.after_render();
+  },
   render: async () => {
     return Products.render();
   },
